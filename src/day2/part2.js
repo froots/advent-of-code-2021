4e3cb6257f6ba65316b6f12f3042bf3e53c56868
Jim Newbery <james@tinnedfruit.com>
2021-12-02 13:24:48 +0000

    Refactor 2.2

--- src/day2/part2.js
+++ src/day2/part2.js
@@ -1,22 +1,19 @@
-const reducer = ([h, d, aim], [direction, dist]) => {
-  switch (direction) {
-    case 'forward':
-      return [h + dist, d + aim * dist, aim];
-    case 'down':
-      return [h, d, aim + dist];
-    case 'up':
-      return [h, d, aim - dist];
-    default:
-      return [h, d, aim];
+const { product } = require('../reducers');
+
+const processInstruction = ([h, d, aim], [dir, dist]) => {
+  if (dir === 'forward') {
+    return [h + dist, d + aim * dist, aim];
   }
+  const aimMod = dir === 'down' ? 1 : -1;
+  return [h, d, aim + aimMod * dist];
 };
 
-const part2 = (input) => {
-  const instructions = input
+const part2 = (input) =>
+  input
     .map((instr) => instr.split(' '))
-    .map(([direction, dist]) => [direction, Number(dist)]);
-  const [h, d] = instructions.reduce(reducer, [0, 0, 0]);
-  return h * d;
-};
+    .map(([direction, dist]) => [direction, Number(dist)])
+    .reduce(processInstruction, [0, 0, 0])
+    .slice(0, 2)
+    .reduce(product);
 
 module.exports = part2;
