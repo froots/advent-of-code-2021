cc94c61a6fc12c7cd4f47243e2aa093a3f759be6
Jim Newbery <james@tinnedfruit.com>
2022-01-01 18:18:10 +0000

    10.2 solution

--- src/day10/solution.js
+++ src/day10/solution.js
@@ -7,11 +7,25 @@ const CLOSE_MAP = {
   '>': '<',
 };
 
+const OPEN_MAP = {
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
+};
+
 const SYNTAX_ERROR_SCORES = {
   ')': 3,
   ']': 57,
   '}': 1197,
   '>': 25137,
+};
+
+const COMPLETION_SCORES = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
 };
 
 const analyse = (line) => {
@@ -53,17 +67,43 @@ const analyse = (line) => {
   return {
     line,
     corrupt,
+    completion:
+      !corrupt &&
+      chunkStack
+        .reverse()
+        .map((opening) => OPEN_MAP[opening])
+        .join(''),
   };
 };
 
-const part1 = (input) =>
+const parse = (input) =>
   input
     .split('\n')
     .map((line) => line.trim())
-    .filter(Boolean)
+    .filter(Boolean);
+
+const completionScore = (completionString) =>
+  completionString
+    .split('')
+    .reduce(
+      (score, completion) => score * 5 + COMPLETION_SCORES[completion],
+      0
+    );
+
+const part1 = (input) =>
+  parse(input)
     .map(analyse)
     .filter((lineObj) => lineObj.corrupt)
     .map((lineObj) => SYNTAX_ERROR_SCORES[lineObj.corrupt])
     .reduce(sum, 0);
 
-module.exports = { part1, analyse };
+const part2 = (input) => {
+  const scores = parse(input)
+    .map(analyse)
+    .filter((lineObj) => !lineObj.corrupt)
+    .map((lineObj) => completionScore(lineObj.completion))
+    .sort((a, b) => a - b);
+  return scores[Math.floor(scores.length / 2)];
+};
+
+module.exports = { part1, part2, analyse, completionScore };
--- src/day10/solution.test.js
+++ src/day10/solution.test.js
@@ -1,4 +1,4 @@
-const { part1, analyse } = require('./solution');
+const { part1, part2, analyse, completionScore } = require('./solution');
 
 const example = `[({(<(())[]>[[{[]{<()<>>
 [(()[<>])]({[<{<<[]>>(
@@ -21,6 +21,7 @@ test('10: Analyse returns correct data object for non-corrupt lines', () => {
   expect(analyse(valid)).toEqual({
     line: '{}[]',
     corrupt: undefined,
+    completion: '',
   });
 });
 
@@ -29,5 +30,27 @@ test('10: Analyse returns correct data object for corrupt lines', () => {
   expect(analyse(invalid)).toEqual({
     line: '{{{[]]}}',
     corrupt: ']',
+    completion: false,
   });
+});
+
+test('10: Analyse returns correct data object for incomplete lines', () => {
+  const incomplete = '{<([()';
+  expect(analyse(incomplete)).toEqual({
+    line: '{<([()',
+    corrupt: undefined,
+    completion: '])>}',
+  });
+});
+
+test('10: lineCompletionScore calculates completion score from completion string', () => {
+  expect(completionScore('}}]])})]')).toBe(288957);
+  expect(completionScore(')}>]})')).toBe(5566);
+  expect(completionScore('}}>}>))))')).toBe(1480781);
+  expect(completionScore(']]}}]}]}>')).toBe(995444);
+  expect(completionScore('])}>')).toBe(294);
+});
+
+test('10.2: Calculates middle completion score of uncompleted lines', () => {
+  expect(part2(example)).toBe(288957);
 });
